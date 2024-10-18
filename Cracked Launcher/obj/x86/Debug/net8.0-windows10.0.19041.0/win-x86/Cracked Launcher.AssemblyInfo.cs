81fd1ea7c54d91edcbe34959832a425da7c168e5
Tsvetan Vagliarski <mvilinowo34@gmail.com>
2024-10-18 21:32:29 +0300

    Fixed Thnigs

--- Cracked Launcher/obj/x86/Debug/net8.0-windows10.0.19041.0/win-x86/Cracked Launcher.AssemblyInfo.cs
+++ Cracked Launcher/obj/x86/Debug/net8.0-windows10.0.19041.0/win-x86/Cracked Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cracked Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b376555d25a49f3f4f6c6dab6ff5de284606ecea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c80c4cb0c5be077b5d37327620bde6ecd9f6493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cracked Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cracked Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
